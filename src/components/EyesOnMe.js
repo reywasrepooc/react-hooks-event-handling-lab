9b22b2f487697e584e73759d82ce3eb8380e6cad
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-07-01 15:12:02 -0400

    finished

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+
+    const eventFocus = () => { console.log('Good!') }
+const eventBlur = () => { console.log('Hey! Eyes on me!') }
+
+return <button onFocus={eventFocus} onBlur={eventBlur}>Eyes on me</button>
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+import React from "react"
+
+const Keypad = () => {
+    const change = () => {
+        console.log('Entering password...')
+    }
+      return <input type='password' onChange={change} />
+}
+export default Keypad
